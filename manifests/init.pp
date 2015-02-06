aa7bb74a345423bb5f8c92b7102b16e8a719bd38
Lorenzo Cocchi <lorenzo.cocchi@softecspa.it>
2015-02-06 15:33:33 +0100

    include php-cli con pin del pacchetto

--- manifests/init.pp
+++ manifests/init.pp
@@ -41,6 +41,7 @@ class ispconfig_packages {
     softec_php::module{ 'ldap':}
   }
 
+  include softec_php::cli
   include softec_php::imagick
   include softec_php::memcache
   include softec_php::uploadprogress
